c5694ad9d494ccdc6efabb8fc625327b17da75bc
achille56 <leclercqjm59@gmail.com>
2022-11-19 21:13:03 +0100

    exo 53

---
+++ script.js
@@ -0,0 +1,92 @@
+let text1 = document.getElementById("text1");
+    text1.style.color = "green";
+    text1.style.width = "25%";
+    text1.style.animationName = "text1et3";
+    text1.style.fontSize = "1.8rem";
+
+let text2 = document.getElementById("text2");
+    text2.style.color = "yellow";
+    text2.style.width = "15%";
+
+let text3 = document.getElementById("text3");
+    text3.style.color = "black";
+    text3.style.width = "20%";
+    text1.style.animationName = "text1et3";
+    text1.style.fontSize = "1.8rem";
+
+let text4 = document.getElementById("text4");
+    text4.style.color = "pink";
+    text4.style.width = "10%";
+
+let text5 = document.getElementById("text5");
+    text5.style.color = "orange";
+    text5.style.width = "30%";
+    text5.style.fontSize = "2.5rem";
+
+let span1 = document.querySelector("#span1");
+    span1.style.background = "blue";
+    span1.dir = "rtl";
+
+let span2 = document.querySelector("#span2");
+    span2.style.background = "yellow";
+    span2.dir = "rtl";
+
+let span3 = document.querySelector("#span3");
+    span3.style.background = "orange";
+    span3.dir = "rtl";
+
+let span4 = document.querySelector("#span4");
+    span4.style.background = "grey";
+    span4.dir = "rtl";
+
+let span5 = document.querySelector("#span5");
+    span5.style.background = "green";
+    span5.dir = "rtl";
+
+let span6 = document.querySelector("#span6");
+    span6.style.background = "pink";
+    span6.dir = "rtl";
+
+let span7 = document.querySelector("#span7");
+    span7.style.background = "green";
+    span7.dir = "rtl";
+
+let span8 = document.querySelector("#span8");
+    span8.style.background = "blue";
+    span8.dir = "rtl";
+
+let span9 = document.querySelector("#span9");
+    span9.style.background = "marron";
+    span9.dir = "rtl";
+
+let span10 = document.querySelector("#span10");
+    span10.style.background = "black";
+    span10.dir = 'rtl';
+
+// les liens
+
+let Gle = document.getElementById("Google");
+    Gle.innerHTML = "Google";
+    Gle.href = "https//www.google.com/";
+
+let JvD = document.getElementById("jeuxVideo");
+    JvD.innerHTML = "jeuxVideo";
+    JvD.href = "https//www.jeuxvideo.fr";
+
+let Inst = document.getElementById("instant-gaming");
+    Inst.innerHTML = "Instant-Gaming";
+    Inst.href = "https//www.instant-gaming.com";
+
+let Mtn = document.getElementById("materiel.net");
+    Mtn.innerHTML = "materiel.net";
+    Mtn.href = "https//www.materiel.net";
+
+let Ama = document.getElementById("amazon");
+    Ama.innerHTML = "Amazon";
+    Ama.href = "https//www.amazon.com";
+
+
+
+
+
+
